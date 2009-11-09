cff24c08afe5c065b9572f6b10518421df785c2f
m0 <m0@viper.internal.network>
2009-11-09 15:34:57 +0000

    Remove old API

--- jack-sample.ads
+++
@@ -1,9 +0,0 @@
-with interfaces.c;
-
-generic package jack.sample is
-  package c renames interfaces.c;
-
-  -- Sample type (must match jack_default_audio_sample_t in jack/types.h)
-  type type_t is new c.c_float;
-
-end jack.sample;
