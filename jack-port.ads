b28cdba92e44168c68dc709cfa5eb3e9887b9786
m0 <m0@viper.internal.network>
2009-11-12 03:10:45 +0000

    Get_Buffer_Address -> Buffer_Address

--- jack-port.ads
+++ jack-port.ads
@@ -8,7 +8,7 @@ package Jack.Port is
   --
 
   -- proc_map : jack_port_get_buffer
-  function Get_Buffer_Address
+  function Buffer_Address
     (Port             : in Client.Port_t;
      Number_Of_Frames : in Client.Number_Of_Frames_t) return System.Address;
 
